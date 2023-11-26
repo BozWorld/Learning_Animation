68ef82c62a5f057bce154de6351f6a80e8cbfb73
Moeva <mondohamoeva@hotmail.fr>
2023-11-26 20:13:04 +0100

    {adding movement}

--- Script/PlayerMovement.gd
+++ Script/PlayerMovement.gd
@@ -4,6 +4,8 @@ class_name Knight
 @export var speed = 5.0
 @export var acceleration = 4.0
 @export var jump_speed = 8.0
+@export var mouse_sensitivity= 0.0015
+@export var rotation_speed = 12.0
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var jumping = false
@@ -17,6 +19,8 @@ func _physics_process(delta):
 	velocity.y += -gravity * delta
 	get_move_input(delta)
 	move_and_slide()
+	if velocity.length() > 1.0:
+		model.rotation.y = lerp_angle(model.rotation.y, spring_arm.rotation.y, rotation_speed * delta)
 
 func get_move_input(delta):
 	var vy = velocity.y
@@ -25,3 +29,9 @@ func get_move_input(delta):
 	var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, spring_arm.rotation.y)
 	velocity = lerp(velocity, dir * speed, acceleration * delta)
 	velocity.y = vy
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		spring_arm.rotation.x -= event.relative.y * mouse_sensitivity
+		spring_arm.rotation_degrees.x = clamp(spring_arm.rotation_degrees.x, -90.0, 30.0)
+		spring_arm.rotation.y -= event.relative.x * mouse_sensitivity
